54d630eeec02c2a126e56078ce6b85074841c818
Sid McLaughlin <sidmclaughlin@gmail.com>
2022-05-15 13:10:59 -0700

    chore(86002:server): configure server entrypoint

--- packages/apps/server/src/main.ts
+++ packages/apps/server/src/main.ts
@@ -1,16 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
+import { Environment } from './common/dtos/environment.dto';
 import { PrismaService } from './common/servivces/prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
-
-  app.useLogger(app.get(Logger));
-
+  const configService: ConfigService<Environment, true> = app.get(ConfigService);
   const prismaService = app.get(PrismaService);
+
   prismaService.enableShutdownHooks(app);
 
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.useLogger(app.get(Logger));
+  app.setGlobalPrefix('api');
+  app.enableVersioning();
+  app.enableCors();
+
+  await app.listen(configService.get('SERVER_PORT'), configService.get('SERVER_HOST'));
 }
 void bootstrap();
