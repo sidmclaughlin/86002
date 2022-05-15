7be67475024a1f089eae1febbc87af61faa7b3ce
Sid McLaughlin <sidmclaughlin@gmail.com>
2022-05-15 11:46:18 -0700

    chore(86002:server): add nestjs-pino

--- packages/apps/server/src/app.module.ts
+++ packages/apps/server/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [LoggerModule.forRoot()],
 })
 export class AppModule {}
