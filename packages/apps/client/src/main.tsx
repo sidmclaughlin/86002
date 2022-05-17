43a4eeed57f6820a65ee792e0f0704a1badefa46
Sid McLaughlin <sidmclaughlin@gmail.com>
2022-05-16 20:03:26 -0700

    chore(86002:client): add dependencies

--- packages/apps/client/src/main.tsx
+++ packages/apps/client/src/main.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet-async';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Helmet titleTemplate="%s | A Paint Company" />
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
