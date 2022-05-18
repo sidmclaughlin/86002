6a5e550302e70a2694d96b1d32c1d08514192af0
Sid McLaughlin <sidmclaughlin@gmail.com>
2022-05-17 20:34:39 -0700

    chore(86002): update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,6 +47,7 @@ const config = {
       rules: {
         '@typescript-eslint/no-empty-function': 0,
         '@typescript-eslint/restrict-template-expressions': 0,
+        '@typescript-eslint/no-misused-promises': 0,
         '@typescript-eslint/no-unsafe-argument': 0,
         '@typescript-eslint/no-unsafe-assignment': 0,
         '@typescript-eslint/no-unsafe-call': 0,
